a5cab743610c77fd09fbc0c2fbfb252d495d84b1
Tomasz Malisiewicz <malist@gmail.com>
2020-03-22 00:07:30 -0700

    Change permission to non-root and check with python3 for success

--- build_pyransac.sh
+++ build_pyransac.sh
@@ -4,4 +4,8 @@ sudo docker build -t pyransac_builder .
 sudo docker run --name pyransac_builder -v `pwd`:/target_directory pyransac_builder
 sudo docker rm pyransac_builder
 
-# Now the pyransac subdirectory contains what you need
+#change permissions so that the final directory is not owned by root
+sudo chown -R `whoami` pyransac
+
+# Now the pyransac subdirectory contains the library (.so file)
+echo import 'pyransac' | python3 - && echo "Installation of pyransac finished" || echo "Installation of pyransac failed"
